c75c92750955ecf3d9d166ba06a541dc3e748d5e
digle228 <digle228@gmail.com>
2016-05-10 21:39:30 +0800

    5/10

---
+++ kidsWorkOut/trackBar.swift
@@ -0,0 +1,21 @@
+//
+//  trackBar.swift
+//  kidsWorkOut
+//
+//  Created by wusuchen on 2016/4/30.
+//  Copyright © 2016年 lakst. All rights reserved.
+//
+
+import UIKit
+
+class trackBar: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
